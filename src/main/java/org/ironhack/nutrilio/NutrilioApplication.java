1831dc41d80c33b19ba3891e0b11eef14dd43b7f
David ZN <davidztrading@gmail.com>
2026-05-30 13:19:00 +0200

    configuracion inicial del proyecto con seguridad, jpa, mysql y spring ai openai

---
+++ src/main/java/org/ironhack/nutrilio/NutrilioApplication.java
@@ -0,0 +1,13 @@
+package org.ironhack.nutrilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ironhack/nutrilio/NutrilioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ironhack.nutrilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
