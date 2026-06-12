21a32a41246b9bda3b2b6cd0811edd67e5bef324
David ZN <davidztrading@gmail.com>
2026-06-12 21:53:09 +0200

    Readme

--- src/main/java/org/ironhack/nutrilio/models/FreshFood.java
+++ src/main/java/org/ironhack/nutrilio/models/FreshFood.java
@@ -3,17 +3,17 @@ package org.ironhack.nutrilio.models;
 import jakarta.persistence.*;
 import lombok.*;
 
-// BLOQUE DE IDENTIFICACIÓN DE SUBCLASE
+
 @Entity
 @DiscriminatorValue("FRESH")
-// BLOQUE DE CONFIGURACIÓN DE LOMBOK PARA HERENCIA
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class FreshFood extends Food {
 
-    // BLOQUE DE ATRIBUTOS EXCLUSIVOS DE ALIMENTOS FRESCOS
+
     private String originCountry;
     private Boolean isOrganic;
 }
--- src/main/java/org/ironhack/nutrilio/repository/UserProfileRepository.java
+++ src/main/java/org/ironhack/nutrilio/repository/UserProfileRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
-    // Este método es el que fallaba porque no estaba declarado explícitamente
-    // Spring ahora entenderá cómo buscar un perfil dado un usuario
     Optional<UserProfile> findByUser(User user);
 }
